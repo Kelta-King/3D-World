17250b6d4e1c521ea081a7119314dbcd46e8076e
Kelta-King <kushangshah131@gmail.com>
2023-05-27 11:05:07 +0530

    updated 3D world init

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require("express");
-const ejs = require("ejs");
 const app = express();
 const port = 3000;
 
-app.set("view engine", ejs);
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
 
 app.listen(port, () => {
     console.log(`Server Listening on ${port}`);
